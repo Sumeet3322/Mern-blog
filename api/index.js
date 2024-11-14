ed130d41227516e34e13b88a2097c3d6246255de
Sumeet3322 <rishubhyadav332255@gmail.com>
2024-11-14 12:45:49 +0530

    Connect to data base

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
+
+const app = express();
 
 app.listen(6969, () => {
-    console.log("Server is running on port 6969!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-})
-
-
-
+  console.log("Server is running on port 6969!");
+});
