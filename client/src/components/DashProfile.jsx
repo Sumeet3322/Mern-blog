d3aeab567cea2207f0734d55ffa9f32aa571cbdd
Sumeet3322 <rishubhyadav332255@gmail.com>
2025-01-03 14:44:48 +0530

    Unfinished Image Upload Because of Pro Firebase

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,48 @@
 import { Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    console.log("uploading image.......");
+  };
+
+  console;
   return (
     <div className="max-w-lg mx-auto p-3 w-full ">
       <h1 className="my-7 font-semibold text-3xl text-center">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="h-32 w-32 rounded-full self-center cursor-pointer shadow-md overflow-hidden ">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="h-32 w-32 rounded-full self-center cursor-pointer shadow-md overflow-hidden "
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full object-cover border-4 border-[lightgray]"
           />
@@ -36,7 +69,7 @@ export default function DashProfile() {
           Update
         </Button>
       </form>
-      <div className="text-red-500 flex justify-between mt-5">
+      <div className="text-red-500 flex justify-between mt-4">
         <span className="cursor-pointer">Delete Account </span>
         <span className="cursor-pointer">Sign Out </span>
       </div>
